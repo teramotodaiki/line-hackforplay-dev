287dc3770aecbddf2e7af115638fa2f3f5c2bd0d
teramotodaiki <i101323@gmail.com>
2017-04-17 14:20:19 +0900

    refactoring

--- index.php
+++ index.php
@@ -8,29 +8,35 @@ $handler = new \Monolog\Handler\SlackWebhookHandler( getenv('SLACK_WEBHOOK_URL')
 $log->pushHandler($handler);
 
 // Webhook by LINE SDK
-
-$httpRequestBody = file_get_contents('php://input'); // Request body string
-$log->debug($httpRequestBody);
-$hash = hash_hmac('sha256', $httpRequestBody, getenv('CHANNEL_SECRET'), true);
-$signature = base64_encode($hash);
-// Compare X-Line-Signature request header string and the signature
-if ($_SERVER['HTTP_X_LINE_SIGNATURE'] !== $signature) {
-    $log->error('X-Line-Signature does not match'); exit;
-}
-$request = json_decode($httpRequestBody);
-// bot client
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+$signature = $_SERVER['HTTP_X_LINE_SIGNATURE']; // X-Line-Signature in header
+$httpRequestBody = file_get_contents('php://input'); // Request body string
 
-foreach ($request->events as $event) {
-    // $event == Webhook event objects https://devdocs.line.me/ja/#webhook-event-object
-    if ($event->type === 'message') {
-        if ($event->message->type === 'text') {
-            // Reply message
-            $text = utf8_strrev($event->message->text); // Reversed text of input
-            $response = $bot->replyText($event->replyToken, $text);
+try {
+    // Compare X-Line-Signature request header string and the signature
+    // Success then parse request body
+    $events = $bot->parseEventRequest($httpRequestBody, $signature);
+
+    foreach ($events as $event) {
+        // $event <Webhook event objects> has a message
+        // See: https://devdocs.line.me/ja/#webhook-event-object
+        if ($event->type === 'message') {
+            if ($event->message->type === 'text') {
+                // Reply message
+                $text = utf8_strrev($event->message->text); // Reversed text of input
+                $bot->replyText($event->replyToken, $text); // Send reply!
+            }
         }
     }
+
+} catch (Exception $e) {
+    // An error was occured, see details in Slack channel!
+    $log->alert('😵', [
+        'message' => $e->getMessage(),
+        'request' => $httpRequestBody,
+    ]);
+
 }
 
 function utf8_strrev($str){
